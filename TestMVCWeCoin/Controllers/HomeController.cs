7bcc886c7a798476e47a66c51a58f73576b4090e
João Pedro <jpfaria19@hotmail.com>
2018-04-08 21:05:34 -0300

    New update.: Edit profile page.

--- TestMVCWeCoin/Controllers/HomeController.cs
+++ TestMVCWeCoin/Controllers/HomeController.cs
@@ -63,18 +63,23 @@ namespace TestMVCWeCoin.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult EditProfile()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
     }
 }
